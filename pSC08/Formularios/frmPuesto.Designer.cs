bdcb4131762082b428c36c1bb1eac47abb025d8d
Rey <reudysfernando@gmail.com>
2024-06-18 10:13:00 -0400

    Nada

--- pSC08/Formularios/frmPuesto.Designer.cs
+++ pSC08/Formularios/frmPuesto.Designer.cs
@@ -86,7 +86,7 @@ namespace pSC08
             this.label1.BackColor = System.Drawing.Color.CornflowerBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(1, 0);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(504, 88);
             this.label1.TabIndex = 9;
